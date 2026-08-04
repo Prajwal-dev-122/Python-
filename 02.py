c9f2f16414a2b0f0ef78feabcf0d12304a895aec
Prajwal <prajwal9626@gmail.com>
2026-08-04 19:47:58 +0530

    Create 02.py

---
+++ 02.py
@@ -0,0 +1,119 @@
+'''  1. What is a Variable?
+
+A variable is a container that stores data in memory.
+
+Think of it like a labeled box:
+
+Name Box  → "Prajwal"
+Age Box   → 20
+Marks Box → 95
+
+In Python:
+
+name = "Prajwal"
+age = 20
+marks = 95
+
+Here,
+
+name is a variable.
+"Prajwal" is the value stored in it.
+2. Variable Syntax
+variable_name = value
+
+Example:
+
+city = "Shivamogga"
+city → Variable
+= → Assignment operator
+"Shivamogga" → Value
+3. Rules for Naming Variables
+Allowed
+name = "Prajwal"
+_age = 20
+marks123 = 95
+student_name = "Rahul"
+Not Allowed
+
+Starts with number
+
+2name = "Prajwal"
+
+Contains space
+
+student name = "Rahul"
+
+Contains special characters
+
+name@ = "Prajwal"
+
+Uses Python keywords
+
+if = 10
+4. Naming Convention
+
+Good
+
+student_name
+first_name
+phone_number
+
+Bad
+
+a
+abc
+x
+
+Use meaningful names.
+
+5. What is a Data Type?
+
+A data type tells Python what kind of value is stored in a variable.
+
+Example
+
+20 → Integer
+20.5 → Float
+"Prajwal" → String
+True → Boolean
+6. Built-in Data Types
+Numeric
+Integer (int)
+
+Whole numbers.
+
+age = 20
+marks = 95
+
+Output
+
+20
+95
+Float (float)
+
+Decimal numbers.
+
+height = 5.8
+price = 99.99
+Complex (complex)
+
+Used in mathematics.
+
+x = 2 + 3j
+Text
+String (str)
+
+Stores text.
+
+name = "Prajwal"
+college = 'KAPMI'
+Boolean
+
+Stores only two values.
+
+is_student = True
+is_pass = False'''
+
+name = "Prajwal"
+age = 20
+marks = 95
